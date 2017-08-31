054b765533b712b6d14d445ff3c2567ca94ef55d
Your Name <you@example.com>
2017-08-31 23:18:43 +0530

    sagar

---
+++ add2.py
@@ -0,0 +1,6 @@
+def add(num1,num2):
+    sum=int(num1)+int(num2);
+    print('the sum of {0} and {1} is {2}'.format(num1,num2,sum));
+num1=input('enter the number\n');
+num2=input('enter the number\n');
+add(num1,num2);
