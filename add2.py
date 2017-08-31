8387fc65f540ee784c0582d8d67b636a73d664ee
Your Name <you@example.com>
2017-08-31 23:27:08 +0530

    sag

--- add2.py
+++ add2.py
@@ -1,6 +1,8 @@
 def add(num1,num2):
-    sum=int(num1)+int(num2);
-    print('the sum of {0} and {1} is {2}'.format(num1,num2,sum));
-num1=input('enter the number\n');
-num2=input('enter the number\n');
-add(num1,num2);
+    return a+b;
+def read():
+    return int(input('enter the number\n'));
+a=read();
+b=read();
+s=add(a,b);
+print('sum of {0}+{1}={2}'.format(a,b,s));
